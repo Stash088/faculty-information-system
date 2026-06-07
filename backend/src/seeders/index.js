bdea782e1d9164aa7a1f7518d10450def88e03a4
Amir <amir@local>
2026-06-07 17:15:09 +0300

    fix: remove undefined ensureContent from exports

--- backend/src/seeders/index.js
+++ backend/src/seeders/index.js
@@ -500,4 +500,4 @@ const seedDatabase = async () => {
   }
 };
 
-module.exports = { seedDatabase, isDataSeeded, seedCategories, seedApplicantContent, ensureContent };
+module.exports = { seedDatabase, isDataSeeded, seedCategories, seedApplicantContent };
